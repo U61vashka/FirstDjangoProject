c50da6167bcf493ca6335da4cb45bc5d6999f5c9
U61vashka <botyrbojey@gmail.com>
2023-02-08 20:31:55 +0600

    Added #2

--- tasks/models.py
+++ tasks/models.py
@@ -7,11 +7,12 @@ class Status(models.TextChoices):
     UNSTARTED = 'u', "Not started yet"
     ONGOING = 'o', "Ongoing"
     FINISHED = 'f', "Finished"
+    FAILED = 'fail', "Failed"
 
 
 class Task(models.Model):
     name = models.CharField(verbose_name="Task name", max_length=65, unique=True)
-    status = models.CharField(verbose_name="Task status", max_length=1, choices=Status.choices)
+    status = models.CharField(verbose_name="Task status", max_length=10, choices=Status.choices)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
