dec0cb02ffd5a70dce33d00b9e2b639c3c72b593
U61vashka <botyrbojey@gmail.com>
2023-02-10 19:37:34 +0600

    Added #3

--- tasks/models.py
+++ tasks/models.py
@@ -13,6 +13,8 @@ class Status(models.TextChoices):
 class Task(models.Model):
     name = models.CharField(verbose_name="Task name", max_length=65, unique=True)
     status = models.CharField(verbose_name="Task status", max_length=10, choices=Status.choices)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
