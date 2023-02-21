763c176665384c9cf5e676d3acfccf92e56ff54d
bivashy <botyrbojey@gmail.com>
2023-02-21 16:30:43 +0600

    Fix typo 'movies'

--- tasks/models.py
+++ tasks/models.py
@@ -13,7 +13,7 @@ class Task(models.Model):
     status = models.CharField(verbose_name="Task status", max_length=10, choices=Status.choices)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    creator = models.ForeignKey('auth.User', related_name='movies', on_delete=models.CASCADE, null=True)
+    creator = models.ForeignKey('auth.User', related_name='tasks', on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return self.name
